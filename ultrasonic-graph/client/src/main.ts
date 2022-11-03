c4fe80cf5cc272720ff6675878facb87b64e300a
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2022-11-02 22:13:04 -0700

    Working graph

--- ultrasonic-graph/client/src/main.ts
+++ ultrasonic-graph/client/src/main.ts
@@ -1,6 +1,8 @@
 import { serviceUuid, characteristicUuid } from '../../ids.json'
 import never from 'never'
 import Chart from 'chart.js/auto'
+import autoColors from 'chartjs-plugin-autocolors'
+Chart.register(autoColors)
 
 const button = document.createElement('button')
 button.innerText = 'requestDevice'
@@ -9,8 +11,41 @@ document.body.appendChild(button)
 const errorP = document.createElement('p')
 document.body.appendChild(errorP)
 
+const canvas = document.createElement('canvas')
+const ctx = canvas.getContext('2d') ?? never()
+
+const datasets: any[] = []
+const chart = new Chart(ctx, {
+  type: 'scatter',
+  data: {
+    datasets
+  },
+  options: {
+    showLine: true,
+    parsing: false,
+    scales: {
+      y: {
+        title: {
+          text: 'Distance (cm)',
+          display: true
+        },
+        min: 0
+      },
+      x: {
+        title: {
+          text: 'Time (seconds)',
+          display: true
+        }
+      }
+    }
+  }
+})
+
+document.body.appendChild(canvas)
+
 button.addEventListener('click', () => {
   (async () => {
+    errorP.innerText = ''
     const device = await navigator.bluetooth.requestDevice({
       filters: [{
         services: [serviceUuid]
@@ -24,41 +59,39 @@ button.addEventListener('click', () => {
     console.log(service)
     const characteristic = await service.getCharacteristic(characteristicUuid)
     console.log(characteristic)
-    const sensorUuids = JSON.parse(
-      new TextDecoder().decode(await characteristic.readValue())) as string[]
+    const { sensorUuids } = JSON.parse(
+      new TextDecoder().decode(await characteristic.readValue())) as {
+      sensorUuids: string[]
+      triggerInterval: number
+    }
 
     console.log(sensorUuids)
 
-    sensorUuids.forEach(() => {
-      const canvas = document.createElement('canvas')
-      const ctx = canvas.getContext('2d') ?? never()
-      const labels = [
-        'January',
-        'February',
-        'March',
-        'April',
-        'May',
-        'June'
-      ]
+    datasets.splice(0)
+    await Promise.all(sensorUuids.map(async (uuid, index) => {
+      const data: any[] = []
+      datasets.push({
+        label: `Sensor ${index}`,
+        data
+      })
 
-      const data = {
-        labels,
-        datasets: [{
-          label: 'My First dataset',
-          backgroundColor: 'rgb(255, 99, 132)',
-          borderColor: 'rgb(255, 99, 132)',
-          data: [0, 10, 5, 2, 20, 30, 45]
-        }]
+      const characteristic = await service.getCharacteristic(uuid)
+      const listener: EventListener = () => {
+        const { distance, time } = JSON.parse(
+          new TextDecoder().decode(characteristic.value ?? never())) as {
+          distance: number
+          time: number
+        }
+        data.push({
+          x: time / 1000,
+          y: distance
+        })
+        chart.update('none')
       }
-
-      const config = {
-        type: 'line',
-        data,
-        options: {}
-      }
-      const myChart = new Chart(ctx, config)
-      document.body.appendChild(canvas)
-    })
+      characteristic.addEventListener('characteristicvaluechanged', listener)
+      await characteristic.startNotifications()
+    }))
+    chart.update()
   })().catch(e => {
     console.error(e)
     errorP.innerText = e.toString()
