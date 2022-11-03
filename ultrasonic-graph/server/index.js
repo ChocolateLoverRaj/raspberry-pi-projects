58c977cbbc714688bb783f0025714d7c0a9441f8
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2022-11-02 22:13:26 -0700

    Working graph

--- ultrasonic-graph/server/index.js
+++ ultrasonic-graph/server/index.js
@@ -3,14 +3,21 @@ import ids from '../ids.json' assert { type: 'json' }
 import parseEnvJsonSchema from 'parse-env-json-schema'
 import { randomUUID } from 'crypto'
 import { config } from 'dotenv'
+import { Gpio } from 'pigpio'
+import parseEnvInt from 'parse-env-int'
 config()
+
+// The number of microseconds it takes sound to travel 1cm at 20 degrees celcius
+const MICROSECDONDS_PER_CM = 1e6 / 34321
+
+const triggerInterval = parseEnvInt('TRIGGER_INTERVAL')
 
 const serviceName = 'com.ultrasonicGraph'
 
 const bp = new BlePeripheral(serviceName, ids.serviceUuid, () => {
   bp.logCharacteristicsIO = true
 
-  const sensors = bp.Characteristic(
+  const sensorsCharacteristic = bp.Characteristic(
     ids.characteristicUuid,
     'sensors',
     ['read'])
@@ -27,7 +34,17 @@ const bp = new BlePeripheral(serviceName, ids.serviceUuid, () => {
 
   const sensorUuids = sensorPins.map(() => randomUUID())
   
-  sensors.setValue(JSON.stringify(sensorUuids))
+  sensorsCharacteristic.setValue(JSON.stringify({
+    sensorUuids,
+    triggerInterval
+  }))
+
+  const sensors = sensorPins.map(([triggerPin, echoPin]) => ({
+    trigger: new Gpio(triggerPin, { mode: Gpio.OUTPUT }),
+    echo: new Gpio(echoPin, { mode: Gpio.INPUT, alert: true })
+  }))
+  const subscribedDevices = new Array(sensorPins.length).fill(0)
+  const watchingDataArr = []
 
   sensorPins.forEach((pins, index) => {
     const sensor = bp.Characteristic(
@@ -37,10 +54,44 @@ const bp = new BlePeripheral(serviceName, ids.serviceUuid, () => {
     )
 
     sensor.on('StartNotify', () => {
-      console.log('Start reading sensor:', index)
+      if (subscribedDevices[index]++ === 0) {
+        sensors[index].trigger.digitalWrite(0)
+        let startTick
+
+        const alertHandler = (level, tick) => {
+          if (level === 1) {
+            startTick = tick
+          } else {
+            const endTick = tick
+            const diff = (endTick >> 0) - (startTick >> 0) // Unsigned 32 bit arithmetic
+            const distanceCm = diff / 2 / MICROSECDONDS_PER_CM
+            sensor.notify(JSON.stringify({
+              distance: distanceCm,
+              time: Date.now() - watchingDataArr[index].startTime
+            }))
+          }
+        }
+        sensors[index].echo.on('alert', alertHandler)
+
+        // FIXME: Bluetooth stops working with low intervals (<100)
+        const interval = setInterval(() => {
+          sensors[index].trigger.trigger(10, 1)
+        }, triggerInterval)
+
+        watchingDataArr[index] = { 
+          alertHandler, 
+          interval, 
+          startTime: Date.now()
+        }
+      }
     })
     sensor.on('StopNotify', () => {
-      console.log('Stop reading sensor:', index)
+      if (--subscribedDevices[index] === 0) {
+        console.log('Stop reading sensor:', index)
+        const { interval, alertHandler } = watchingDataArr[index]
+        sensors[index].echo.off('alert', alertHandler)
+        clearInterval(interval)
+      }
     })
   })
 })
---
+++ ultrasonic-graph/server/start.sh
@@ -0,0 +1,1 @@
+sudo "$(which node)" index.js
